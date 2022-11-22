0bc35c7a98808fe8ab1e61662e63a7a022cedf89
Jack Ecuyer <58667720+JackEcuyer@users.noreply.github.com>
2022-11-22 20:56:22 +0000

    configured tailwindcss
    configured and tested tailwindcss is working, created login route

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Track My Workout</h1>
+      <Link href="/login">
+        <h1 className="text-blue-500">Login</h1>
+      </Link>
     </div>
   );
 }
